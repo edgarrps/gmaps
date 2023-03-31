edfd8cd257a9beeeaee95fa754734612a0b07470
edgarrps <edgarrps@gmail.com>
2023-03-30 22:19:28 -0400

    changed to leaflet map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { Map } from '@/components/Map'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
-<h1 className='bg-red-600 text-white'>MAPA</h1>
+    <>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" />
+      <Map />
+    </>
   )
 }
