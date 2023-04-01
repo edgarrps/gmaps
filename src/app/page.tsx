8f4afcaf00180d54c95cfcc9e73757fc29a81372
edgarrps <edgarrps@gmail.com>
2023-03-31 21:44:34 -0400

    leaflet-map css + js added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
+'use client'
 import { Map } from '@/components/Map'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
   return (
     <>
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" />
       <Map />
     </>
   )
