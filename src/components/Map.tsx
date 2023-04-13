35481dbbff55f253b2c12a5548af37fa3d9c9676
edgarrps <edgarrps@gmail.com>
2023-04-13 16:13:55 -0400

    create a coordinate const

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,14 +2,16 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 
 export default function App() {
+
+  const coord: [number, number] = [-15.822630, -47.920683]
   return (
     <div>
-      <MapContainer center={[-15.822630, -47.920683]} zoom={3} className='h-screen'>
+      <MapContainer center={(coord)} zoom={3} className='h-screen'>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[-15.822630, -47.920683]}>
+        <Marker position={coord}>
         </Marker>
       </MapContainer>
     </div>
