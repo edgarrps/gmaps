7c74c80d31155e0f202ae31378a38a90efe9c26f
edgarrps <edgarrps@gmail.com>
2023-04-13 16:08:04 -0400

    Marker in Brazil

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,15 @@
 'use client'
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 
 export default function App() {
   return (
     <div>
-      <MapContainer center={[0, 0]} zoom={3} className='h-screen'>
+      <MapContainer center={[-15.822630, -47.920683]} zoom={3} className='h-screen'>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[0, 0]}>
-          <Popup>
-            Project by Edgar Santos
-          </Popup>
+        <Marker position={[-15.822630, -47.920683]}>
         </Marker>
       </MapContainer>
     </div>
