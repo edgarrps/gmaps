a69f242f11cab216676067e1c6c79c45835409de
edgarrps <edgarrps@gmail.com>
2023-04-14 10:09:44 -0400

    Flex division of display

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,8 +6,9 @@ export default function App() {
   const coord: [number, number] = [-15.822630, -47.920683]
   
   return (
-    <div>
-      <MapContainer center={(coord)} zoom={3} className='h-screen'>
+    <div className='flex'>
+      <div className='w-[25%] h-[800px] bg-stone-200'/>
+      <MapContainer center={(coord)} zoom={3} className='h-[800px] w-[80%] bottom-[50%]'>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
