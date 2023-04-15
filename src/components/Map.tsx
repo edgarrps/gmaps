e5773e0b18395c4010136ac80f20b67f7c12f340
Edgar Santos <edgarrps@gmail.com>
2023-04-14 23:10:34 -0400

    created a new style for bottom div

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,23 +1,24 @@
 'use client'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 export default function App() {
 
   const coord: [number, number] = [-15.822630, -47.920683]
-  
+
   return (
-    <div className='flex'>
-      <div className='flex-initial w-[400px] h-[800px] bg-stone-200'/>
-      <MapContainer center={(coord)} zoom={3} className='flex-1 w-64 h-[800px] bottom-[50%]'>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={coord}>
-        </Marker>
-      </MapContainer>
-    </div>
-  )
+    <>
+      <div className='flex h-[650px]'>
+        <div className='flex-initial w-[400px] bg-stone-200'></div>
+        <MapContainer center={(coord)} zoom={12} className='flex-1 w-64'>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
+      <div className='h-[230px] overflow-y-auto bg-blue-200'/>
+      </>
+      )
 }
 
 
