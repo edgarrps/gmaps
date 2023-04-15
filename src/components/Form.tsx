4b45b6c5cf71afbfe89807f61dd6b3489be16083
Edgar Santos <edgarrps@gmail.com>
2023-04-15 13:15:33 -0400

    rendered the first input component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,8 @@ import Input from "./Input"
 
 export default function Form () {
     return(
-<></>
+    <form className='grid place-items-center space-y-5 pt-10'>
+        <Input type='text' placeholder='teste' className='mt-4 rounded-full pl-3 w-80 h-8' />
+    </form>
     )
 }
