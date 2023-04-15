f66985657979792f7f48917e4ca28a2c47c659a7
Edgar Santos <edgarrps@gmail.com>
2023-04-15 13:25:11 -0400

    created button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import { ButtonProps } from "@/types/ButtonProps";
+
+export default function Button ({className,text}:ButtonProps) {
+    return (
+        <button className={className}>{text}</button>
+    )
+}
