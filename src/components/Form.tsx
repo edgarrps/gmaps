9ddf6fe2eaaf392f0b6b791ff9d352e97cdf188d
Edgar Santos <edgarrps@gmail.com>
2023-04-18 23:11:15 -0400

    Changed placeholder button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,7 @@ export default function Form () {
     <form className='grid place-items-center space-y-5 pt-10'>
         <Input type='text' placeholder='origem' className='mt-4 rounded-full pl-3 w-80 h-8' />
         <Input type='text' placeholder='destino' className='mt-4 rounded-full pl-3 w-80 h-8' />
-        <Button text='botão' className='text-white font-bold bg-blue-400 w-20 rounded-full hover:bg-blue-300'/>
+        <Button text='Calcular' className='text-white font-bold bg-blue-400 w-20 rounded-full hover:bg-blue-300'/>
     </form>
     )
 }
